d4b696a9d2cfd04f279c8d740e69f7291a6448c6
blahajcoding <venusisonvenus@gmail.com>
2026-03-02 20:00:10 +0000

    The countdown begins.

---
+++ script.js
@@ -0,0 +1,29 @@
+const mElement = document.getElementById("months");
+
+var countDownDate = new Date("May 14, 2026 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor((distance / (1000 * 60 * 60 * 24)));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("months").innerHTML = months;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "0";
+    document.getElementById("hours").innerHTML = "0";
+    document.getElementById("minutes").innerHTML = "0";
+    document.getElementById("seconds").innerHTML = "0";
+    document.getElementById(months).innerHTML = "0";
+  }
+}, 1000);
